fa2354482ba1c12ab1afc8747d65c4459856a459
JaiVeer-creative <73180396+JaiVeer-creative@users.noreply.github.com>
2020-10-31 16:29:03 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+#computers are dum
+#we define functions in python program 
+#some functions are inbuilt,some are user defined
+def hello(n):
+ return n
+Varp=hello(5)
+print(Varp)
+ 
+def hi(a,p):
+  c = a+p
+ 
+  return c
+o = hi(120,140)
+print(o)
