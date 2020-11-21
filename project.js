ae4e1f2531beb9fd47e46896335dadbb4a2eecdf
JaiVeer-creative <73180396+JaiVeer-creative@users.noreply.github.com>
2020-11-22 02:38:10 +0530

    Create project.js

---
+++ project.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas(480, 600);
+}
+function draw() {
+background(4450);
+beginShape();
+vertex(40, 120);  
+vertex(40, 190);
+vertex(430, 190);
+vertex(430, 120)
+line(00,00,50, 150);
+line(480,00,430,120);
+  line(0,00,50, 150);
+endShape()
+fill(390)}
